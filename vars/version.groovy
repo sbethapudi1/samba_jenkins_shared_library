76188269f65a531bcbf32bf2892630a3129f55c1
Sambaiah <samba.bethapudi@gmail.com>
2021-03-18 21:02:47 +0530

    addding another groovy files

--- vars/version.groovy
+++ vars/version.groovy
@@ -1,6 +1,6 @@
 def call(String projectname)
 {
-	if (projectname == "DCA_Enabler")
+	if (projectname == "WESUTILS")
 	{
 	def RESET_REL_VERSION= datas.compile.RESET_REL_VERSION
 	sh """
@@ -9,8 +9,8 @@ def call(String projectname)
 		then
 		echo "In the IF Section"
 		sed -i "s\\(^PACKAGE_RELEASE=\\V).*\\1\$RESET_REL_VERSION/" /root/version_enabler.txt;
-		sed -i "s\\(^PKG_RELEASE=\\)./\\1\$RESET_REL_VERSION/" \${WORKSPACE }/DCA_ENABLER/INFO ;
-		sed -i "s/ (^PKG_RELEASE=\\)./\\1\$RESET_REL_VERSION/" \$(WORKSPACE}/DCA_ENABLER_PACKAGES/INFO
+		sed -i "s\\(^PKG_RELEASE=\\)./\\1\$RESET_REL_VERSION/" \${WORKSPACE }/WESUTILS/INFO ;
+		sed -i "s/ (^PKG_RELEASE=\\)./\\1\$RESET_REL_VERSION/" \$(WORKSPACE}/WESUTILS_PACKAGES/INFO
 		else
 		/root/version_enabler.txt
 		echo "Now Sourcing PACKAGE_RELEASE number"
@@ -18,8 +18,8 @@ def call(String projectname)
 		varInca'expr \$PACKAGE RELEASE + 01
 		echo "Package release version is \$varInc"
 		sed -i "s\\(PACKAGE_RELEASE-11). */\\1\$varinc/" /root/version_enabler.txt;
-		sed -i "s\\(APKG_RELEASE=\\).*/\\1\$varinc/" \${WORKSPACE}/DCA_ENABLER/INFO ;
-		sed -i "s/(PKG_RELEASE=\\).*/\\1\$varinc/" \${WORKSPACE}/DCA_ENABLER_PACKAGES/INFO;
+		sed -i "s\\(APKG_RELEASE=\\).*/\\1\$varinc/" \${WORKSPACE}/WESUTILS/INFO ;
+		sed -i "s/(PKG_RELEASE=\\).*/\\1\$varinc/" \${WORKSPACE}/WESUTILS_PACKAGES/INFO;
 		echo "\$varInc"> /root/env_version.txt
 		fi
 	"""
