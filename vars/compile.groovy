9d4b4ad7312a592e1b35ee220aeb55451497124f
Sambaiah <samba.bethapudi@gmail.com>
2021-03-18 14:31:28 +0530

    addding another groovy files

--- vars/compile.groovy
+++ vars/compile.groovy
@@ -2,7 +2,7 @@ def call(String buildtool, String makeParams, String projectname, String buildir
 //script{ datas = readyaml (file: 'jenkins.yaml') )
 if ( buildtool == "make" ) {
 //previously ($datas.compile.makeParams), ${datas.projectname}, $datas.compile buildir)), projectname doesnt exist in yaml
-make makeParams, projectname, buildir)
+make(makeParams, projectname, buildir)
 } 
 else if ( buildtool == "msbuild" ) {
 msbuild( makeParams, projectname, buildir)
