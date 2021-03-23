aea0a7a77d8d3e2aa74b6419923cac578fa81845
Sambaiah <samba.bethapudi@gmail.com>
2021-03-23 08:55:31 +0530

    working one standard pipeline

--- src/org/acme/stdPipeline.groovy
+++ src/org/acme/stdPipeline.groovy
@@ -3,27 +3,6 @@ package org.acme;
 
 def execute() {
 
-  node {
 
-    stage('Initialize') {
-      checkout scm
-      echo 'Loading pipeline definition'
-      Yaml parser = new Yaml()
-      Map pipelineDefinition = parser.load(new File(pwd() + '/pipeline.yaml').text)
-    }
-
-    switch(pipelineDefinition.pipelineType) {
-      case 'python':
-        // Instantiate and execute a Python pipeline
-        new pythonPipeline(pipelineDefinition).executePipeline()
-      case 'nodejs':
-        // Instantiate and execute a NodeJS pipeline
-        new nodeJSPipeline(pipelineDefinition).executePipeline()
-      case 'msbuild':
-        // Instantiate and execute a Msbuild pipeline
-        new MsbuildPipeline(pipelineDefinition).executePipeline()
-    }
-
-  }
 
 }
