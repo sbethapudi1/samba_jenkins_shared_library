b5af89c3007071e6b26aa95cbab4659a9cf6a263
Sambaiah <samba.bethapudi@gmail.com>
2021-03-23 08:50:17 +0530

    working one standard pipeline

--- src/org/foo/Point.groovy
+++
@@ -1,7 +0,0 @@
-// src/org/foo/Point.groovy
-package org.foo
-
-// point in 3D space
-class Point {
-  float x,y,z
-}
--- src/org/foo/Zot.groovy
+++
@@ -1,8 +0,0 @@
-// src/org/foo/Zot.groovy
-package org.foo
-
-def checkOutFrom(repo) {
-  git url: "git@github.com:jenkinsci/${repo}"
-}
-
-return this
