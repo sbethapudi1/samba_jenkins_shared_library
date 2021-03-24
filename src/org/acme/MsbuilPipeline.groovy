0c1058c13470bfd167c633af2985c9015fa72e77
Sambaiah <samba.bethapudi@gmail.com>
2021-03-24 09:09:36 +0530

    changed Standard pipeline..

--- src/org/acme/MsbuilPipeline.groovy
+++ src/org/acme/MsbuilPipeline.groovy
@@ -9,18 +9,20 @@ def call() {
     def p = pipelineCfg()
 
     if (p.runTests == true) {
-      docker.image(p.testImage).inside() {
+      
         stage('Test') {
           sh 'pip install -r requirements.txt'
           sh p.testCommand
         }
-      }
+      
     }
 
     if (env.BRANCH_NAME == 'master' && p.deployUponTestSuccess == true) {
-      docker.image(p.deployToolImage).inside {
+      
         stage('Deploy') {
           sh "echo ${p.deployCommand} ${p.deployEnvironment}"
         }
-      }
+      
     }
+	
+	}
